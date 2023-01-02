40043026504e796eda544e0a868d5afa6d512c8a
igor2519 <igorkoklipkov@gmail.com>
2023-01-02 17:00:28 +0200

    Removed log

--- src/functions/scheduled/handler.ts
+++ src/functions/scheduled/handler.ts
@@ -2,10 +2,9 @@ import { remindersService } from '@services';
 import debug from 'debug';
 
 const logger = debug('app:error');
-logger('KURVA ');
+
 export const sendReminders = async () => {
   try {
-    logger('HERE');
     await remindersService.sendEvents();
   } catch (e) {
     logger(e);
